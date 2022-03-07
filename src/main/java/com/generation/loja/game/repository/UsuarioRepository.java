558d53a0010733b3573071c937cdadf1e6ddf019
mayaratlt <mayaratlt23@gmail.com>
2022-03-07 19:24:16 -0300

    Loja Game Com Security

---
+++ src/main/java/com/generation/loja/game/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.generation.loja.game.repository;
+
+import java.util.Optional;
+
+import com.generation.loja.game.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
